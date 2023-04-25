b49f70c407e962dec7d9ee169d74394ab61d73ad
Chris Gibbons <christ487@icloud.com>
2023-04-25 20:18:26 +0100

    added JS for menu btn on mobile, open, close.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const featureBtn = document.querySelector(".feature-btn")
 const featureMenu = document.querySelector(".feature-menu")
 const companyBtn = document.querySelector(".company-btn")
 const companyMenu = document.querySelector(".company-menu")
+const openMenuBtn = document.querySelector(".open-menu-btn")
+const mobileMenuBtn = document.querySelector(".close-mobile-menu-btn")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 featureBtn.addEventListener("click", () => {
     featureMenu.classList.toggle("hidden");
@@ -10,3 +13,12 @@ featureBtn.addEventListener("click", () => {
 companyBtn.addEventListener("click", () => {
     companyMenu.classList.toggle("hidden");
 })
+
+openMenuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+})
+
+mobileMenuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+})
+
